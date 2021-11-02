880ae96c1ccd39693ea4fa50b06b7bff32a4da90
Koh Jun Jie <koh.jun.jie@outlook.com>
2021-11-02 18:05:29 +0800

    Added eslint configuration

--- models/person.js
+++ models/person.js
@@ -1,51 +1,52 @@
 const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
 
+// eslint-disable-next-line no-undef
 const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 
 mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch(error => {
-        console.log('error connecting to MongoDB: ', error.message)
-    })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB: ', error.message)
+  })
 
 const checkDigit = character => {
-    if (character >= '0' && character <= '9')
-        return true
-    else
-        return false
+  if (character >= '0' && character <= '9')
+    return true
+  else
+    return false
 }
 
 const personSchema = new mongoose.Schema({
-    name: { type: String, required: true, unique: true, minlength: 3 },
-    number: { 
-        type: String, 
-        required: true, 
-        unique: true, 
-        validate: {
-            validator: v => {
-                let count = 0
-                for (const element of v) {
-                    if (checkDigit(element))
-                        count++;
-                }
-                
-                return count >= 8
-            },
-            message: props => `${props.value} does not contain at least eight digits!`
-        }}
+  name: { type: String, required: true, unique: true, minlength: 3 },
+  number: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: v => {
+        let count = 0
+        for (const element of v) {
+          if (checkDigit(element))
+            count++
+        }
+
+        return count >= 8
+      },
+      message: props => `${props.value} does not contain at least eight digits!`
+    } }
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 personSchema.plugin(uniqueValidator)
--- mongo.js
+++ mongo.js
@@ -1,43 +1,44 @@
+/* eslint-disable no-undef */
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log('Please provide the password as an argument: node mongo.js <password>')
-    process.exit(1)
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
 }
 
 const password = process.argv[2]
 
-const url = 
+const url =
     `mongodb+srv://admin:${password}@mycluster.xnorb.mongodb.net/phonebook?retryWrites=true&w=majority`
 
 mongoose.connect(url)
 
 const contactSchema = new mongoose.Schema({
-    name: String,
-    number: String
+  name: String,
+  number: String
 })
 
 const Contact = new mongoose.model('Person', contactSchema)
 
 const contact = new Contact({
-    name: process.argv[3],
-    number: process.argv[4]
+  name: process.argv[3],
+  number: process.argv[4]
 })
 
 
-if (process.argv.length == 5) {
-    contact.save().then(result => {
-        console.log(result)
-        console.log(`added ${contact.name} number ${contact.number} to phonebook`)
-        mongoose.connection.close()
+if (process.argv.length === 5) {
+  contact.save().then(result => {
+    console.log(result)
+    console.log(`added ${contact.name} number ${contact.number} to phonebook`)
+    mongoose.connection.close()
+  })
+} else if (process.argv.length === 3) {
+  Contact.find({}).then(result => {
+    result.forEach(contact => {
+      console.log(contact)
     })
-} else if (process.argv.length == 3) {
-    Contact.find({}).then(result => {
-        result.forEach(contact => {
-            console.log(contact)
-        })
-        mongoose.connection.close()
-    })
+    mongoose.connection.close()
+  })
 }
 
 
