39c0586d823363246990e57dac2abb1f8a151065
Koh Jun Jie <koh.jun.jie@outlook.com>
2021-11-02 17:23:46 +0800

    Completed exercise 3.20 of Part 3

--- models/person.js
+++ models/person.js
@@ -13,9 +13,31 @@ mongoose.connect(url)
         console.log('error connecting to MongoDB: ', error.message)
     })
 
+const checkDigit = character => {
+    if (character >= '0' && character <= '9')
+        return true
+    else
+        return false
+}
+
 const personSchema = new mongoose.Schema({
-    name: { type: String, required: true, unique: true },
-    number: String
+    name: { type: String, required: true, unique: true, minlength: 3 },
+    number: { 
+        type: String, 
+        required: true, 
+        unique: true, 
+        validate: {
+            validator: v => {
+                let count = 0
+                for (const element of v) {
+                    if (checkDigit(element))
+                        count++;
+                }
+                
+                return count >= 8
+            },
+            message: props => `${props.value} does not contain at least eight digits!`
+        }}
 })
 
 personSchema.set('toJSON', {
